1139947295ea48263cc6ce16d0903e5221de39ed
AdminKTS <161103013+AdminKTS@users.noreply.github.com>
2024-02-25 11:02:34 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+ function home(){
+            window.location.href="index.html";
+ }
+ function about(){
+    window.location.href="index.html#about";
+ }
+ function activity(){
+    window.location.href="activity.html"; 
+ }
+ function contact(){
+     window.location.href="index.html#contact_main_and";
+ }
+ function donate(){
+    window.location.href="donation.html";
+ }
+ function ga() {
+    window.location.href="gallery.html";
+ }
